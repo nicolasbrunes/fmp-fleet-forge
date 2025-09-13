13882e0edfefee9bf55024b9787fb90910ded8ba
NICOLAS YAGO NETO OLIVEIRA BRUNES <nicolas.brunes@aluno.fmpsc.edu.br>
2025-09-13 12:54:10 -0300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/fmp-fleet-forge/", // <--- ADICIONE ESTA LINHA
   server: {
     host: "::",
     port: 8080,
